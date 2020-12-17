b1123d15edb8e3f5806d34066bb514f973dc11a9
Dark-Wolf15 <72570655+Dark-Wolf15@users.noreply.github.com>
2020-12-17 07:49:52 -0600

    Update Reclamos.java

--- Proyecto/src/proyecto/Reclamos.java
+++ Proyecto/src/proyecto/Reclamos.java
@@ -15,14 +15,13 @@ import javax.swing.JOptionPane;
 
 public class Reclamos 
 {
-    
     //Atributos
     private String nombre;
     private String apellidos;
     private int cedula;
-    private int fecha;
+    private String fecha;
     private String descripcion;
-    private int articulo;
+    private String articulo;
     
     
     public String getNombre() {
@@ -48,32 +47,49 @@ public class Reclamos
     public void setCedula(int cedula) {
         this.cedula = cedula;
     }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getArticulo() {
+        return articulo;
+    }
+
+    public void setArticulo(String articulo) {
+        this.articulo = articulo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+    
+    
     
     //Metodo Reclamo
     public void ReclamoP()
     {
-      JOptionPane.showInputDialog("Aqui usted podrá hacer el reclamo de su articulo");
-      fecha= Integer.parseInt(JOptionPane.showInputDialog("En que fecha hizo la compra de su articulo con el formato MM/DD/YYYY:"));
+  
+      fecha= JOptionPane.showInputDialog("En que fecha hizo la compra de su articulo con el formato MM/DD/YYYY:");
       nombre=JOptionPane.showInputDialog("Ingrese su nombre:");
       apellidos=JOptionPane.showInputDialog("Ingrese sus apellidos:");
       cedula=Integer.parseInt(JOptionPane.showInputDialog("Ingrese su numero de cedula:"));
-      articulo=Integer.parseInt(JOptionPane.showInputDialog("Digite el nombre del articulo con el que quieras hacer el reclamo:"));
+      articulo=JOptionPane.showInputDialog("Digite el nombre del articulo con el que quieras hacer el reclamo:");
       descripcion=JOptionPane.showInputDialog("Porque desea hacer el reclamo del articulo:"); 
    
-      JOptionPane.showInputDialog("Usted a realizado el reclamo:"
+      JOptionPane.showMessageDialog(null,"Usted a realizado el reclamo:"
                                   +"\nFecha de Compra: "+fecha
                                   +"\nArticulo: "+articulo
                                   +"\nMotivo: "+descripcion
                                   +"\nNombre"+nombre+apellidos
                                   +"\nCédula :"+cedula);
-    
-            
-            
-    }
-}
-
-
-
-
-
-
